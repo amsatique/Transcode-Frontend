6abacc35b15103b9d997d145766ca4894d092d4d
amsatique <amsatique@gmail.com>
2016-02-29 16:18:09 +0100

    ezrfzfe

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,6 @@
 #!/bin/bash
 set -e
 
-<<<<<<< HEAD
-#Install Dependencies
-=======
-#MONGODB_NAME=${MONGODB_NAME}
-#MONGODB_USER=${MONGODB_USER}
-#MONGODB_PASS=${MONGODB_PASS}
-#PRIVATE_KEY=${PRIVATE_KEY}
-#EMAIL=${EMAIL}
-#EMAIL_NAME=${EMAIL_NAME}
-#EMAIL_PASSWD=${EMAIL_PASSWD}
-
-#Add app config
-#sed -i -e "s/\"db\": \"YourDataBaseName\"/\"db\": ${MONGODB_NAME}/g" /app/config/parameters.json
-#sed -i -e "s/\"username\": \"YourDataBaseUserName\"/\"username\": ${MONGODB_USER}/g" /app/config/parameters.json
-#sed -i -e "s/\"password\": \"YourDatabasePassword\"/\"password\": ${MONGODB_PASS}/g" /app/config/parameters.json
-#sed -i -e "s/\"privateKey\": \"YourPrivateKey\"/\"privateKey\": ${PRIVATE_KEY}/g" /app/config/parameters.json
-#sed -i -e "s/\"email\": \"senderMail@website.com\"/\"email\": ${EMAIL}/g" /app/config/parameters.json
-#sed -i -e "s/\"userName\": \"YourMailAccount\"/\"username\": ${EMAIL_NAME}/g" /app/config/parameters.json
-#sed -i -e "s/\"password\": \"YourMailPassword\"/\"password\": ${EMAIL_PASSWD}/g" /app/config/parameters.json
-
->>>>>>> origin/master
 cd /app
 npm install
 
